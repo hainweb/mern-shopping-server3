5e31068c4e8c18780859c95ec7ac7a91b3afb42c
hainweb <hainofficialweb@gmail.com>
2024-10-27 11:13:28 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,25 @@
 // config/connection.js
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI || 'mongodb+srv://ajinrajeshhillten:5PeT8NxReh3zCwou@shoppingcart.jv3gz.mongodb.net/?retryWrites=true&w=majority&appName=ShoppingCart';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+});
 
 const connect = (callback) => {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ajinrajeshhillten:5PeT8NxReh3zCwou@shoppingcart.jv3gz.mongodb.net/?retryWrites=true&w=majority&appName=ShoppingCart', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000
-  })
-  .then(() => {
-    console.log('Database Connected Successfully');
-    if (callback) callback(null);
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err);
-    if (callback) callback(err);
-  });
+  client.connect()
+    .then(() => {
+      console.log('Database Connected Successfully');
+      if (callback) callback(null, client);
+    })
+    .catch((err) => {
+      console.error('Database connection error:', err);
+      if (callback) callback(err);
+    });
 };
 
-module.exports = { connect };
-
-// In app.js
-db.connect((err) => {
-  if (err) {
-    console.log('Database connection failed:', err);
-  } else {
-    console.log('Database Connected Successfully');
-  }
-});
+module.exports = { connect, client };
